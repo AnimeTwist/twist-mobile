8f68f684ac9a323faeaf6eb57cd0f211230d671f
ATechnoHazard <amolele@gmail.com>
2019-10-14 00:07:00 +0530

    Add utils
    * Constants singleton for project-wide constants
    * Extension functions for common tasks like toasts
    * A preference helper for easier use of shared prefs

---
+++ app/src/main/java/dev/smoketrees/twist/utils/Constants.kt
@@ -0,0 +1,8 @@
+package dev.smoketrees.twist.utils
+
+object Constants {
+
+    const val PACKAGE_NAME = "dev.smoketrees.twist"
+
+    const val PREF = "$PACKAGE_NAME.pref"
+}
---
+++ app/src/main/java/dev/smoketrees/twist/utils/Extensions.kt
@@ -0,0 +1,47 @@
+package dev.smoketrees.twist.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun ViewGroup.inflate(layoutRes: Int): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, false)
+}
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun Fragment.hideKeyboard() {
+    view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Activity.hideKeyboard() {
+    if (currentFocus == null) View(this) else currentFocus?.let { hideKeyboard(it) }
+}
+
+fun Context.hideKeyboard(view: View) {
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Fragment.toast(message: String) {
+    requireContext().toast(message)
+}
