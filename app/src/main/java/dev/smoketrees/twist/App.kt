ad6ef5e2c44885c44a9272f4313bba3cc30a43b8
ATechnoHazard <amolele@gmail.com>
2019-10-13 22:46:58 +0530

    Use custom application class
    This will be useful in the future when we use Koin for Dependency Injection
    
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

---
+++ app/src/main/java/dev/smoketrees/twist/App.kt
@@ -0,0 +1,17 @@
+package dev.smoketrees.twist
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+        }
+    }
+}
