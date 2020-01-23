606695f1b87fb5b94c461de6620841c1df60ff0c
Till Kottmann <me@deletescape.ch>
2020-01-23 08:17:50 +0100

    Replace hardcoded constant with build config info
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- app/src/main/java/dev/smoketrees/twist/utils/Constants.kt
+++ app/src/main/java/dev/smoketrees/twist/utils/Constants.kt
@@ -1,8 +1,7 @@
 package dev.smoketrees.twist.utils
 
+import dev.smoketrees.twist.BuildConfig
+
 object Constants {
-
-    const val PACKAGE_NAME = "dev.smoketrees.twist"
-
-    const val PREF = "$PACKAGE_NAME.pref"
+    const val PREF = "${BuildConfig.APPLICATION_ID}.pref"
 }
