bdf3feadae18078983b7389daaadfdd010b12788
Shreyansh Lodha <slodha96@gmail.com>
2020-03-16 20:28:04 +0530

    Implement day night theme with toggle
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

--- app/src/main/java/dev/smoketrees/twist/ui/home/MainActivity.kt
+++ app/src/main/java/dev/smoketrees/twist/ui/home/MainActivity.kt
@@ -1,29 +1,35 @@
 package dev.smoketrees.twist.ui.home
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
-import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.ContextCompat
+import androidx.core.content.edit
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import dev.smoketrees.twist.R
+import dev.smoketrees.twist.utils.Constants
 import dev.smoketrees.twist.utils.hide
 import dev.smoketrees.twist.utils.show
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
 
 class MainActivity : AppCompatActivity() {
 
     private val navController by lazy { (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).findNavController() }
 
+    private val pref by inject<SharedPreferences>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkAndApplyTheme()
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
@@ -44,4 +50,30 @@ class MainActivity : AppCompatActivity() {
     fun showLoader() = spinkit.show()
 
     fun hideLoader() = spinkit.hide(500)
+
+    private fun checkAndApplyTheme() {
+        if (pref.contains(Constants.PreferenceKeys.IS_DAY)) { // Check if this pref exists or not, if not then check what theme does system have
+            if (pref.getBoolean(Constants.PreferenceKeys.IS_DAY, true)) { // Day
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else { // Night
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        } else { // Check what theme does system follow
+            if (systemIsDark()) { // System in dark
+                pref.edit { putBoolean(Constants.PreferenceKeys.IS_DAY, false) }
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else { // System in light
+                pref.edit { putBoolean(Constants.PreferenceKeys.IS_DAY, true) }
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    private fun systemIsDark(): Boolean {
+        return when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false // Assume day
+        }
+    }
 }
